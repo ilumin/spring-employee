a4bc6a40539e86987071e0424c0c86bfad844c5b
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-30 15:42:48 +0700

    UPDATE rename class from DemoApplication to Application

---
+++ src/main/java/com/ilumin/lab/Application.java
@@ -0,0 +1,20 @@
+package com.ilumin.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		System.setProperty("spring.config.location", "file:/etc/app/scm/");
+		return builder.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
