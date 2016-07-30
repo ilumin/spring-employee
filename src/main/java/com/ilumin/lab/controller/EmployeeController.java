dcd5d127eafe9080f5f21095115d6a0e873f66df
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-30 20:40:38 +0700

    UPDATE employee controller use page with default page = 1 and size = 20

--- src/main/java/com/ilumin/lab/controller/EmployeeController.java
+++ src/main/java/com/ilumin/lab/controller/EmployeeController.java
@@ -3,8 +3,11 @@ package com.ilumin.lab.controller;
 import com.ilumin.lab.domain.Employee;
 import com.ilumin.lab.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
@@ -17,8 +20,12 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     @RequestMapping(method = RequestMethod.GET)
-    public Iterable<Employee> getEmployees() {
-        return employeeRepository.findAll();
+    public Iterable<Employee> getEmployees(
+            @RequestParam(name = "page", defaultValue = "1", required = false) Integer page,
+            @RequestParam(name = "size", defaultValue = "20", required = false) Integer size
+    ) {
+        Pageable pageable = new PageRequest(page<=0 ? 0 : page - 1, size);
+        return employeeRepository.findAll(pageable);
     }
 
 }
