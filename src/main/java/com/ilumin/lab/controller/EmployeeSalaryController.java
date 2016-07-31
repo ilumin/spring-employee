67d0ad8a47b4497469f0cd720fa59625d52e7fe6
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-31 13:40:01 +0700

    UPDATE employee salary / title controller apply hateoas

--- src/main/java/com/ilumin/lab/controller/EmployeeSalaryController.java
+++ src/main/java/com/ilumin/lab/controller/EmployeeSalaryController.java
@@ -3,7 +3,12 @@ package com.ilumin.lab.controller;
 import com.ilumin.lab.domain.Salary;
 import com.ilumin.lab.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeSalaryController {
@@ -12,12 +17,18 @@ public class EmployeeSalaryController {
     private EmployeeService employeeService;
 
     @RequestMapping(value = "/employees/{id}/salaries", method = RequestMethod.GET)
-    public Iterable<Salary> showSalaries(@PathVariable Integer id) {
-        return employeeService.showSalaries(id);
+    public Resource<Iterable<Salary>> showSalaries(@PathVariable Integer id) {
+        return responseAsResource(id, employeeService.showSalaries(id));
     }
 
     @RequestMapping(value = "/employees/{id}/salaries", method = RequestMethod.POST)
-    public Iterable<Salary> addSalary(@PathVariable Integer id, @RequestBody Salary salaryData) throws Exception {
-        return employeeService.addSalary(id, salaryData);
+    public Resource<Iterable<Salary>> addSalary(@PathVariable Integer id, @RequestBody Salary salaryData) throws Exception {
+        return responseAsResource(id, employeeService.addSalary(id, salaryData));
+    }
+
+    private Resource<Iterable<Salary>> responseAsResource(Integer id, Iterable<Salary> salaries) {
+        Link self = linkTo(methodOn(EmployeeSalaryController.class).showSalaries(id)).withSelfRel();
+        Link employee = linkTo(methodOn(EmployeeController.class).getEmployee(id)).withSelfRel();
+        return new Resource<>(salaries, self, employee);
     }
 }
--- src/main/java/com/ilumin/lab/controller/EmployeeTitleController.java
+++ src/main/java/com/ilumin/lab/controller/EmployeeTitleController.java
@@ -3,7 +3,12 @@ package com.ilumin.lab.controller;
 import com.ilumin.lab.domain.Title;
 import com.ilumin.lab.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeTitleController {
@@ -12,13 +17,19 @@ public class EmployeeTitleController {
     private EmployeeService employeeService;
 
     @RequestMapping(value = "/employees/{id}/titles", method = RequestMethod.GET)
-    public Iterable<Title> showTitles(@PathVariable Integer id) {
-        return employeeService.showTitles(id);
+    public Resource<Iterable<Title>> showTitles(@PathVariable Integer id) {
+        return responseAsResource(id, employeeService.showTitles(id));
     }
 
     @RequestMapping(value = "/employees/{id}/titles", method = RequestMethod.POST)
-    public Iterable<Title> addTitle(@PathVariable Integer id, @RequestBody Title titleData) throws Exception {
-        return employeeService.addTitle(id, titleData);
+    public Resource<Iterable<Title>> addTitle(@PathVariable Integer id, @RequestBody Title titleData) throws Exception {
+        return responseAsResource(id, employeeService.addTitle(id, titleData));
+    }
+
+    private Resource<Iterable<Title>> responseAsResource(Integer id, Iterable<Title> titles) {
+        Link self = linkTo(methodOn(EmployeeTitleController.class).showTitles(id)).withSelfRel();
+        Link employee = linkTo(methodOn(EmployeeController.class).getEmployee(id)).withSelfRel();
+        return new Resource<>(titles, self, employee);
     }
 
 }
