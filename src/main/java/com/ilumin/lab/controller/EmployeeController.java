748da59bda69399a9c94c0c7f3aee590f569d125
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-31 12:47:38 +0700

    UPDATE employee controller add link to single resource

--- src/main/java/com/ilumin/lab/controller/EmployeeController.java
+++ src/main/java/com/ilumin/lab/controller/EmployeeController.java
@@ -1,17 +1,30 @@
 package com.ilumin.lab.controller;
 
 import com.ilumin.lab.domain.Employee;
+import com.ilumin.lab.domain.Salary;
+import com.ilumin.lab.domain.Title;
 import com.ilumin.lab.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 @RestController
+@ExposesResourceFor(Employee.class)
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @Autowired
+    EntityLinks entityLinks;
 
     @RequestMapping(value = "/employees", method = RequestMethod.GET)
     public Iterable<Employee> getEmployees(
@@ -23,10 +36,10 @@ public class EmployeeController {
     }
 
     @RequestMapping(value = "/employees/{id}", method = RequestMethod.GET)
-    public Employee getEmployee(
+    public Resource<Employee> getEmployee(
             @PathVariable Integer id
     ) {
-        return employeeService.getEmployee(id);
+        return responseAsResource(employeeService.getEmployee(id));
     }
 
     @RequestMapping(value = "/employees", method = RequestMethod.POST)
@@ -47,4 +60,10 @@ public class EmployeeController {
         employeeService.deleteEmployee(id);
     }
 
+    private Resource<Employee> responseAsResource(Employee employee) {
+        Link self = linkTo(methodOn(EmployeeController.class).getEmployee(employee.getEmpNo())).withSelfRel();
+        Link titles = linkTo(methodOn(EmployeeTitleController.class).showTitles(employee.getEmpNo())).withRel("all-titles");
+        Link salaries = linkTo(methodOn(EmployeeSalaryController.class).showSalaries(employee.getEmpNo())).withRel("all-salaries");
+        return new Resource<>(employee, self, titles, salaries);
+    }
 }
